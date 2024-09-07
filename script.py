a39360e2ec7c854c3c25f06745f7651566b5e790
prasanna-konduri <prasanna.chinnu10@gmail.com>
2024-09-08 01:36:56 +0530

    updated gitignore file

--- script.py
+++ script.py
@@ -5,6 +5,6 @@ checker = CommitChecker("commits.json")
 
 if checker.check_new_commits() :
     print("Found new commits running the script to pull and deploy")
-    subprocess.run(["./copy_files.sh"], shell=True)
+    subprocess.run(["/Users/sasidharambatipudi/Devops/cicdpipeline/copy_files.sh"], shell=True)
 else:
     print("no new commits found")
